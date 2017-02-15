87d83fcf8727a0aee3c13f4d15c81fdf9f8d17b1
Timothy Jones <Timothy Jones>
2017-02-15 14:44:10 -0700

    Initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Brewing Cookbook
+
+A simple web application for creating and storing brewing recipes.
+
+## How to use this
+  * Install the dependencies: `npm install`
+  * Serve content locally: `npm run serve`
+  * build js bundle: `npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  resolve: {
+    root: [
+      __dirname,
+      path.resolve(__dirname, 'app'),
+      path.resolve(__dirname,'node_modules')
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolveLoader: {
+    root: path.join(__dirname,'node_modules')
+  },
+  devtool: 'soucemaps',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+      inject: 'body',
+      hash: 'true'
+    })
+  ]
+};
