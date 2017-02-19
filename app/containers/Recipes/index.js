4a5d2758c8586d5be00231b10d2f83868238c7bb
Timothy Jones <timothygenejones@gmail.com>
2017-02-18 18:40:58 -0700

    Get Recipes from service and update state

--- app/containers/Recipes/index.js
+++ app/containers/Recipes/index.js
@@ -4,18 +4,24 @@ import React from 'react';
 import NavBar from 'components/navbar';
 import RecipeList from './recipeList.js';
 
-const recipes = [
-  {name: 'New Blue Moon'},
-  {name: 'FatTire'},
-  {name: 'Prost Weissbier'}
-];
+import recipeService from 'services/recipe.js';
 
 export default class Recipes extends React.Component {
   constructor() {
     super();
+    this.state = {
+      recipes: []
+    }
+    recipeService.getRecipes().then((recipes) => {
+      console.log('retrieved recipes: ', recipes);
+      this.setState({
+        recipes: recipes
+      })
+    });
   }
 
   render() {
+    const recipes = this.state.recipes.slice();
     return (
       <div>
         <NavBar title="Brewing Cookbook" />
