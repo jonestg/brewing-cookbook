390d4e21b4923fca9139b146b54b34b2b511a9dc
Timothy Jones <timothygenejones@gmail.com>
2017-02-20 21:32:56 -0700

    Added an Ingredient Service

---
+++ app/services/ingredients.js
@@ -0,0 +1,30 @@
+'use strict';
+import Fuse from 'fuse.js';
+
+var fuse = new Fuse(recipes, {
+  keys: ["name"]
+});
+
+var ingredients = [
+  {id: 0, name: 'hops'},
+  {id: 1, name: 'barley'},
+  {id: 2, name: 'wheat'},
+  {id: 3, name: 'malt'},
+  {id: 4, name: 'yeast'}
+];
+
+var externalApi = {
+  getIngredients: getIngredients
+}
+
+export default externalApi;
+
+function getIngredients(searchTerm) {
+  return new Promise((resolve) => {
+    if(searchTerm) {
+      resolve(fuse.search(searchTerm));
+    } else {
+      resolve(ingredients);
+    }
+  });
+}
