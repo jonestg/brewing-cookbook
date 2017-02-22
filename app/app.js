6202fad5b544ba48ed30783ead911cd252f91bc4
Timothy Jones <timothygenejones@gmail.com>
2017-02-21 21:48:28 -0700

    Removed some unused imports

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Redirect, Route, Link, hashHistory} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Recipes from 'containers/Recipes';
 import CreateRecipe from 'containers/createRecipe';
