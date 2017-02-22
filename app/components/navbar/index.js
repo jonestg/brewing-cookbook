359e80a615dbae8f6716aac6a16091573e701e24
Timothy Jones <timothygenejones@gmail.com>
2017-02-21 21:46:42 -0700

    Added a navigation menu to the nav bar

--- app/components/navbar/index.js
+++ app/components/navbar/index.js
@@ -1,14 +1,50 @@
 'use strict';
 
 import React from 'react';
+import {Link} from 'react-router';
 import AppBar from 'material-ui/AppBar';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
-const NavBar = ({title}) => (
-  <AppBar title={title} />
-);
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+  }
 
-NavBar.propTypes = {
-  title: React.PropTypes.string.isRequired
+  handleMenuClick(event) {
+    this.setState({
+      open: !this.state.open,
+      anchorEl: event.currentTarget
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title={this.props.title}
+          onLeftIconButtonTouchTap={this.handleMenuClick.bind(this)}
+        />
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+        >
+          <Menu>
+            <MenuItem
+              primaryText="Search Recipes"
+              containerElement={<Link to="/" />}
+            />
+            <MenuItem
+              primaryText="Create a Recipe"
+              containerElement={<Link to="/create" />}
+            />
+          </Menu>
+        </Popover>
+      </div>
+    );
+  }
 }
-
-export default NavBar;
